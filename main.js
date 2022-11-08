f25176ec2dc0d79b0feda19170839c62f5fb246c
Rebecca Barrett <barrett4467@gmail.com>
2022-11-07 22:05:34 -0700

    Adding basic styles and starting logic for button submission

---
+++ main.js
@@ -0,0 +1,15 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation();
+});
+
+let formValidation = () => {
+
+}; 
